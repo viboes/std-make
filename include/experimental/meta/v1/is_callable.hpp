4d8402bc98c5e18aa42061cca75ff67ade6fdf2b
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2016-07-03 10:08:14 +0200

    Avoid the instantiation of default_delete() in gcc by specializing Fn() when meta::is_callable<default_delete(void)>.

--- include/experimental/meta/v1/is_callable.hpp
+++ include/experimental/meta/v1/is_callable.hpp
@@ -24,12 +24,18 @@ inline namespace v1
   namespace detail {
     template <class Sig, class R, class = void>
     struct is_callable : false_type {};
-    template <class Fn, class ...U>
-    struct is_callable<Fn(U...), void, void_<typename Fn::template invoke<U...>>>
+    template <class Fn, class U, class ...Us>
+    struct is_callable<Fn(U, Us...), void, void_<typename Fn::template invoke<U, Us...>>>
     : true_type {};
-    template <class Fn, class ...U, class R>
-    struct is_callable<Fn(U...), R, void_<typename Fn::template invoke<U...>>>
-    : is_convertible<invoke<Fn, U...>, R> {  };
+    template <class Fn>
+    struct is_callable<Fn(), void, void_<typename Fn::template invoke<void>>>
+    : true_type {};
+    template <class Fn, class U, class ...Us, class R>
+    struct is_callable<Fn(U, Us...), R, void_<typename Fn::template invoke<U, Us...>>>
+    : is_convertible<invoke<Fn, U, Us...>, R> {  };
+    template <class Fn, class R>
+    struct is_callable<Fn(), R, void_<typename Fn::template invoke<void>>>
+    : is_convertible<invoke<Fn, void>, R> {  };
 
     template <class TC, class = void>
     struct is_invokable : false_type {};
@@ -46,8 +52,14 @@ inline namespace v1
   template <class Fn, class ...U, class R >
   struct is_callable<Fn(U...), R> : detail::is_callable<Fn(U...), R> {};
 
+  template <class Sig, class R = void>
+  constexpr bool is_callable_v = meta::is_callable<Sig, R>::value;
+
   template <class TC>
   struct is_invokable : detail::is_invokable<TC> {};
+
+  template <class TC>
+  constexpr bool is_invokable_v = meta::is_invokable<TC>::value;
 
 }
 }
