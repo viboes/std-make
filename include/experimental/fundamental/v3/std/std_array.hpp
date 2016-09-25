0cba23213b4958bb62242dce17226f727e300dd4
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2016-09-25 17:47:31 +0200

    Add std::make_array.

--- include/experimental/fundamental/v3/std/std_array.hpp
+++ include/experimental/fundamental/v3/std/std_array.hpp
@@ -14,6 +14,32 @@
 
 namespace std
 {
+#if ! defined JASEL_HAS_MAKE_ARRAY
+
+  namespace details {
+    template<class> struct is_ref_wrapper : false_type {};
+    template<class T> struct is_ref_wrapper<reference_wrapper<T>> : true_type {};
+
+    //template<class T>
+    //using not_ref_wrapper = experimental::negation<is_ref_wrapper<decay_t<T>>>;
+
+    template <class D, class...> struct return_type_helper { using type = D; };
+    template <class... Types>
+    struct return_type_helper<void, Types...> : common_type<Types...> {
+        //static_assert(experimental::conjunction_v<not_ref_wrapper<Types>...>,
+        //              "Types cannot contain reference_wrappers when D is void");
+    };
+
+    template <class D, class... Types>
+    using return_type = array<typename return_type_helper<D, Types...>::type,
+                                   sizeof...(Types)>;
+  }
+
+  template < class D = void, class... Types>
+  constexpr details::return_type<D, Types...> make_array(Types&&... t) {
+    return {{forward<Types>(t)... }};
+  }
+#endif
 
 namespace experimental
 {
