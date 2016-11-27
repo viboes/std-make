a9e5b29aee1ecd186048522b478bdd5431cf1e07
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2016-11-27 16:19:10 +0100

    Added missing file.

---
+++ include/experimental/fundamental/v3/nullable/bind.hpp
@@ -0,0 +1,72 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2016.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file // LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//////////////////////////////////////////////////////////////////////////////
+#if __cplusplus >= 201402L
+
+#ifndef JASEL_FUNDAMENTAL_V3_NULLABLE_BIND_HPP
+#define JASEL_FUNDAMENTAL_V3_NULLABLE_BIND_HPP
+
+#include <experimental/fundamental/v2/config.hpp>
+#include <experimental/fundamental/v3/nullable/none.hpp>
+#include <experimental/make.hpp>
+#include <experimental/meta.hpp>
+#include <experimental/type_constructible.hpp>
+#include <experimental/fundamental/v3/monad/monad.hpp>
+#include <utility>
+#include <functional>
+
+namespace std
+{
+namespace experimental
+{
+inline  namespace fundamental_v3
+{
+
+namespace nullable
+{
+
+  /**
+   * bind for TypeConstructible Nullable types
+   */
+
+  template <class M, class F
+  // todo add constraint on F
+  //, class = enable_if_t<
+  //    is_nullable_v<decay_t<N>>
+  // && is_type_constructible_v<decay_t<N>>
+  //>
+  >
+  constexpr auto
+  bind(M&& m, F&& f) -> decltype(
+      std::forward<F>(f)(nullable::deref(forward<M>(m)))
+      )
+
+  {
+    if (nullable::has_value(forward<M>(m)))
+      return
+#if defined JASEL_HAS_INVOKE
+          invoke(std::forward<F>(f), nullable::deref(forward<M>(m)))
+#else
+          std::forward<F>(f)(nullable::deref(forward<M>(m)))
+#endif
+      ;
+    else
+      return nullable::deref_none(forward<M>(m));
+  }
+  struct as_monad: monad::tag
+  {
+    template <class M, class F>
+      static constexpr auto bind(M&& x, F&& f)
+      {
+        return nullable::bind(forward<M>(x), forward<F>(f));
+      }
+  };
+}
+}}
+}
+#endif // header
+#endif
