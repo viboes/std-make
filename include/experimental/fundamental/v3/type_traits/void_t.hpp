476f62c81039d15bedb8628f0d45ab96029c0e8f
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-01-21 15:21:54 +0100

    Moved  void_t to a file.

---
+++ include/experimental/fundamental/v3/type_traits/void_t.hpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2016-2017.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file // LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef JASEL_V3_FUNDAMENTALTYPE_TRAITS_VOID_T_HPP
+#define JASEL_V3_FUNDAMENTALTYPE_TRAITS_VOID_T_HPP
+
+#include <type_traits>
+#include <experimental/meta.hpp>
+
+namespace std {
+  namespace experimental {
+#if __cplusplus > 201402L
+    using std::void_t;
+#else
+    using std::experimental::meta::void_t;
+#endif
+  }
+}
+
+#endif // header
--- include/experimental/type_traits.hpp
+++ include/experimental/type_traits.hpp
@@ -2,7 +2,7 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-// (C) Copyright 2016 Vicente J. Botet Escriba
+// (C) Copyright 2016-2017 Vicente J. Botet Escriba
 
 #ifndef JASEL_EXPERIMENTAL_TYPE_TRAITS_HPP
 #define JASEL_EXPERIMENTAL_TYPE_TRAITS_HPP
@@ -10,16 +10,6 @@
 #include <type_traits>
 
 #include <experimental/fundamental/v3/type_traits/conjunction.hpp>
-#include <experimental/meta.hpp>
-
-namespace std {
-  namespace experimental {
-#if __cplusplus > 201402L
-    using std::void_t;
-#else
-    using std::experimental::meta::void_t;
-#endif
-  }
-}
+#include <experimental/fundamental/v3/type_traits/void_t.hpp>
 
 #endif // header
