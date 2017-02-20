bdfc79e235ff7397cc7c5bc5efaa5abc9c9010da
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-02-20 08:34:09 +0100

    Added more test for ordinal_set.

--- test/ordinal/container/ordinal_set/ordinal_set_pass.cpp
+++ test/ordinal/container/ordinal_set/ordinal_set_pass.cpp
@@ -19,14 +19,144 @@ int main()
   using Indx = Bounded<1,4,unsigned char>;
   using OSet = stde::ordinal_set<Indx>;
 
-  {
+  {// Default Constructor
     OSet os;
-    os[Indx{1}]=true;
-    BOOST_TEST(os[Indx{1}]);
+    BOOST_TEST( 0 == os.count() );
+    BOOST_TEST( os.none() );
+    BOOST_TEST(! os[Indx{1}]);
+    BOOST_TEST(! os[Indx{2}]);
   }
   { // Constructible from Indx
-    OSet e(Indx{2});
-    BOOST_TEST(e[Indx{2}]==true);
+    OSet os(Indx{2});
+    BOOST_TEST( 1 == os.count() );
+    BOOST_TEST( os.any() );
+    BOOST_TEST(os[Indx{2}]==true);
+  }
+  { // operator [T]
+    OSet os;
+    os[Indx{1}]=true;
+    BOOST_TEST( 1 == os.count() );
+    BOOST_TEST( os.any() );
+    BOOST_TEST(os[Indx{1}]);
+  }
+  { // set
+    OSet os;
+    os.set();
+    BOOST_TEST( 4 == os.count() );
+    BOOST_TEST( os.all() );
+    BOOST_TEST(os[Indx{1}]);
+    BOOST_TEST(os[Indx{2}]);
+    BOOST_TEST(os[Indx{3}]);
+    BOOST_TEST(os[Indx{4}]);
+  }
+  { // set
+    OSet os;
+    os.set(Indx{1});
+    BOOST_TEST( 1 == os.count() );
+    BOOST_TEST( os.any() );
+    BOOST_TEST(os[Indx{1}]);
+    BOOST_TEST(! os[Indx{2}]);
+    BOOST_TEST(! os[Indx{3}]);
+    BOOST_TEST(! os[Indx{4}]);
+  }
+  { // set
+    OSet os;
+    os.set(Indx{1});
+    os.set(Indx{1}, false);
+    BOOST_TEST( 0 == os.count() );
+    BOOST_TEST( os.none() );
+    BOOST_TEST(! os[Indx{1}]);
+    BOOST_TEST(! os[Indx{2}]);
+    BOOST_TEST(! os[Indx{3}]);
+    BOOST_TEST(! os[Indx{4}]);
+  }
+  { // reset
+    OSet os;
+    os.set(Indx{1});
+    os.set(Indx{3});
+    os.reset();
+    BOOST_TEST( 0 == os.count() );
+    BOOST_TEST( os.none() );
+    BOOST_TEST(! os[Indx{1}]);
+    BOOST_TEST(! os[Indx{2}]);
+    BOOST_TEST(! os[Indx{3}]);
+    BOOST_TEST(! os[Indx{4}]);
+  }
+  { // reset
+    OSet os;
+    os.set(Indx{1});
+    os.set(Indx{4});
+    os.reset(Indx{1});
+    BOOST_TEST( 1 == os.count() );
+    BOOST_TEST( os.any() );
+    BOOST_TEST(! os[Indx{1}]);
+    BOOST_TEST(! os[Indx{2}]);
+    BOOST_TEST(! os[Indx{3}]);
+    BOOST_TEST( os[Indx{4}]);
+  }
+  { // flip
+    OSet os;
+    os.flip();
+    BOOST_TEST( os.all() );
+    BOOST_TEST( os[Indx{1}] );
+    BOOST_TEST( os[Indx{2}] );
+    BOOST_TEST( os[Indx{3}] );
+    BOOST_TEST( os[Indx{4}] );
+  }
+  { // to_ulong
+    OSet os;
+    auto x = os.to_ulong();
+    BOOST_TEST( 0 == x);
+  }
+  { // to_ulong
+    OSet os;
+    os.set(Indx{1});
+    auto x = os.to_ulong();
+    BOOST_TEST( 1 == x);
+  }
+  { // operator~
+    OSet os;
+    auto os2 = ~os;
+    BOOST_TEST( os.none() );
+    BOOST_TEST( os2.all() );
+    BOOST_TEST(! os[Indx{1}]);
+    BOOST_TEST(! os[Indx{2}]);
+    BOOST_TEST(! os[Indx{3}]);
+    BOOST_TEST(! os[Indx{4}]);
+    BOOST_TEST( os2[Indx{1}]);
+    BOOST_TEST( os2[Indx{2}]);
+    BOOST_TEST( os2[Indx{3}]);
+    BOOST_TEST( os2[Indx{4}]);
+  }
+  { // operator==
+    OSet os1;
+    OSet os2;
+    BOOST_TEST( os1 == os2 );
+  }
+  { // operator!=
+    OSet os1;
+    os1.set(Indx{1});
+    OSet os2;
+    BOOST_TEST( os1 != os2 );
+  }
+  { // explicit CopyConstructor
+    OSet os1;
+    os1.set(Indx{1});
+    OSet os2{os1};
+    BOOST_TEST( os1 == os2 );
+  }
+  { // implicit CopyConstructor
+    OSet os1;
+    os1.set(Indx{1});
+    OSet os2=os1;
+    BOOST_TEST( os1 == os2 );
+  }
+  { // operator=
+    OSet os1;
+    os1.set(Indx{1});
+    OSet os2;
+    os2=os1;
+    BOOST_TEST( os1 == os2 );
   }
 #endif
   return ::boost::report_errors();
