b772907d8caee8faf41032b32dea35bf690e7094
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-04-09 15:56:26 +0200

    format

--- include/experimental/fundamental/v3/strong/not_null.hpp
+++ include/experimental/fundamental/v3/strong/not_null.hpp
@@ -24,10 +24,22 @@ inline  namespace fundamental_v3
   {
       Ptr ptr;
   public:
-      explicit not_null(Ptr p) : ptr(::std::move(p)) {}
-      explicit not_null(::std::nullptr_t) = delete;
+      static_assert(is_assignable<Ptr&, nullptr_t>::value, "T cannot be assigned nullptr.");
+
+      // todo add conversion from convertible to Ptr
+      explicit not_null(Ptr p) : ptr(move(p))
+      {
+        //Expects(ptr != nullptr);
+      }
+      not_null(not_null const&) = default;
+      not_null& operator=(not_null const&) = default;
+      // todo add conversion from not_null<U>
+
+      not_null(nullptr_t) = delete;
+
       Ptr& underlying() { return ptr; }
       Ptr const& underlying() const { return ptr; }
+
       auto operator*() const JASEL_DECLTYPE_RETURN_NOEXCEPT(*ptr)
       auto operator*()  JASEL_DECLTYPE_RETURN_NOEXCEPT(*ptr)
       auto operator->() const JASEL_DECLTYPE_RETURN_NOEXCEPT(::std::addressof(*ptr))
@@ -39,11 +51,15 @@ inline  namespace fundamental_v3
   {
       T* ptr;
   public:
-      explicit not_null(T* p) : ptr(p) {}
+      explicit not_null(T* p) : ptr(p)
+      {
+        //Expects(ptr != nullptr);
+      }
       not_null(T& r) : ptr(&r) {}
-      explicit not_null(::std::nullptr_t) = delete;
+      explicit not_null(nullptr_t) = delete;
       T*& underlying() { return ptr; }
       T* const& underlying() const { return ptr; }
+
       auto operator*() const JASEL_DECLTYPE_RETURN_NOEXCEPT(*ptr)
       auto operator*()  JASEL_DECLTYPE_RETURN_NOEXCEPT(*ptr)
       auto operator->() const JASEL_DECLTYPE_RETURN_NOEXCEPT(::std::addressof(*ptr))
@@ -53,7 +69,7 @@ inline  namespace fundamental_v3
   template <typename Ptr>
   not_null<Ptr> as_not_null(Ptr p)
   {
-      return not_null<Ptr>{::std::move(p)};
+      return not_null<Ptr>{move(p)};
   }
   template <typename T>
   not_null<T*> as_not_null(T& r)
@@ -61,7 +77,7 @@ inline  namespace fundamental_v3
       return not_null<T*>{r};
   }
 
-  void as_not_null(::std::nullptr_t) = delete;
+  void as_not_null(nullptr_t) = delete;
 
 
 
