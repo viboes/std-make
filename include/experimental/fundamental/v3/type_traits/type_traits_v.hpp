c4eb296a4e99dc3d169693a5d9dcab0d02935705
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-04-09 16:02:43 +0200

    added missing file.

---
+++ include/experimental/fundamental/v3/type_traits/type_traits_v.hpp
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2016.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file // LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef JASEL_V3_FUNDAMENTAL_TYPE_TRAITS_TYPE_TRAITS_V__HPP
+#define JASEL_V3_FUNDAMENTAL_TYPE_TRAITS_TYPE_TRAITS_V__HPP
+
+#include <type_traits>
+
+namespace std
+{
+namespace experimental
+{
+inline namespace fundamental_v3
+{
+
+#if __cplusplus >= 201402L
+  template <class R1, class R2>
+  //inline C++17
+  constexpr bool is_constructible_v = is_constructible<R1,R2>::value;
+
+  template <class R1, class R2>
+  //inline C++17
+  constexpr bool is_convertible_v = is_convertible<R1,R2>::value;
+
+#endif
+
+}
+}
+}
+#endif // header
