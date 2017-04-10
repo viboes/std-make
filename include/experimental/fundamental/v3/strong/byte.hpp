fc2a9ef28ed0faf5eed37ea89bafa4b69b2885f6
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-04-11 01:55:29 +0200

    Added byte.

---
+++ include/experimental/fundamental/v3/strong/byte.hpp
@@ -0,0 +1,194 @@
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// Copyright (C) 2017 Vicente J. Botet Escriba
+
+#ifndef JASEL_FUNDAMENTAL_V3_STRONG_BYTE_HPP
+#define JASEL_FUNDAMENTAL_V3_STRONG_BYTE_HPP
+
+#include <experimental/fundamental/v3/strong/underlying_type.hpp>
+#include <experimental/fundamental/v3/strong/wrapper.hpp>
+#include <experimental/fundamental/v2/config.hpp>
+
+#if defined __clang__ && __cplusplus > 201402L
+#define JASEL_HAVE_ENUM_CLASS_CONSTRUCTION_FROM_UNDERLYING_TYPE
+#endif
+
+namespace std
+{
+namespace experimental
+{
+inline  namespace fundamental_v3
+{
+#if defined JASEL_HAVE_ENUM_CLASS_CONSTRUCTION_FROM_UNDERLYING_TYPE
+    enum class byte : unsigned char {};
+
+    template <class IntegerType= unsigned int, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr IntegerType to_integer(byte b) noexcept
+    {
+      return static_cast<IntegerType>(b);
+    }
+
+    inline constexpr unsigned char to_uchar( byte b ) noexcept
+    {
+        return to_integer<unsigned char>( b );
+    }
+    inline constexpr unsigned char to_uchar( int i ) noexcept
+    {
+        return static_cast<unsigned char>( i );
+    }
+
+    template< class T >
+    inline constexpr byte to_byte( T v ) noexcept
+    {
+      return byte{ to_uchar(v) };
+    }
+
+    template <class IntegerType, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr byte& operator<<=(byte& b, IntegerType shift) noexcept
+    {
+      return b = byte{ to_uchar( b ) << shift };
+    }
+    template <class IntegerType, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr byte operator<<(byte b, IntegerType shift) noexcept
+    {
+      return byte{ to_uchar( to_uchar( b ) << shift ) };
+    }
+    template <class IntegerType, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr byte& operator>>=(byte& b, IntegerType shift) noexcept
+    {
+      return b = byte{ to_uchar( b ) >> shift };
+    }
+
+    template <class IntegerType, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr byte operator>>(byte b, IntegerType shift) noexcept
+    {
+      return byte{ to_uchar( b ) >> shift };
+    }
+
+    constexpr byte& operator|=(byte& l, byte r) noexcept
+    {
+      return l = byte{ to_uchar( to_uchar( l ) | to_uchar( r ) ) };
+    }
+    constexpr byte operator|(byte l, byte r) noexcept
+    {
+      return byte{ to_uchar( to_uchar( l ) | to_uchar( r ) ) };
+    }
+    constexpr byte& operator&=(byte& l, byte r) noexcept
+    {
+      return l = byte{ to_uchar( to_uchar( l ) & to_uchar( r ) ) };
+    }
+    constexpr byte operator&(byte l, byte r) noexcept
+    {
+      return byte{ to_uchar( to_uchar( l ) & to_uchar( r ) ) };
+    }
+    constexpr byte& operator^=(byte& l, byte r) noexcept
+    {
+      return l = byte{ to_uchar( to_uchar( l ) ^ to_uchar( r ) ) };
+    }
+    constexpr byte operator^(byte l, byte r) noexcept
+    {
+      return byte{ to_uchar( to_uchar( l ) ^ to_uchar( r ) ) };
+    }
+    constexpr byte operator~(byte b) noexcept
+    {
+      return byte{ to_uchar( ~ to_uchar( b ) ) };
+    }
+#else
+    struct byte : protected_wrapper<unsigned char>
+    {
+      using base_type = protected_wrapper<unsigned char>;
+      using base_type::base_type;
+
+      //!@{
+      //! relational operators
+      //!
+      //! Forwards to the underlying value
+      friend constexpr bool operator==(byte x, byte y)  noexcept { return x.value == y.value; }
+      friend constexpr bool operator!=(byte x, byte y)  noexcept { return x.value != y.value; }
+      friend constexpr bool operator<(byte x, byte y)  noexcept { return x.value < y.value; }
+      friend constexpr bool operator>(byte x, byte y)  noexcept { return x.value > y.value; }
+      friend constexpr bool operator<=(byte x, byte y)  noexcept { return x.value <= y.value; }
+      friend constexpr bool operator>=(byte x, byte y)  noexcept { return x.value >= y.value; }
+      //!@}
+    };
+
+    template< class T >
+    inline constexpr byte to_byte( T v ) noexcept
+    {
+      return { static_cast<byte>( v ) };
+    }
+
+    template <class IntegerType = unsigned int, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr IntegerType to_integer(byte b) noexcept
+    {
+      return static_cast<IntegerType>(b.underlying());
+    }
+
+    inline constexpr unsigned char to_uchar( byte b ) noexcept
+    {
+        return b.underlying();
+    }
+    inline constexpr unsigned char to_uchar( int i ) noexcept
+    {
+        return static_cast<unsigned char>( i );
+    }
+
+    template <class IntegerType, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr byte& operator<<=(byte& b, IntegerType shift) noexcept
+    {
+      return b = to_byte( to_uchar( b ) << shift );
+    }
+    template <class IntegerType, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr byte operator<<(byte b, IntegerType shift) noexcept
+    {
+      return to_byte( to_uchar( b ) << shift );
+    }
+    template <class IntegerType, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr byte& operator>>=(byte& b, IntegerType shift) noexcept
+    {
+      return b = to_byte( to_uchar( b ) >> shift );
+    }
+
+    template <class IntegerType, typename = enable_if<is_integral<IntegerType>::value>>
+    constexpr byte operator>>(byte b, IntegerType shift) noexcept
+    {
+      return to_byte( to_uchar( b ) >> shift );
+    }
+
+    JASEL_MUTABLE_CONSTEXPR byte& operator|=(byte& l, byte r) noexcept
+    {
+      return l = to_byte( to_uchar( l ) | to_uchar( r ) );
+    }
+    constexpr byte operator|(byte l, byte r) noexcept
+    {
+      return to_byte( to_uchar( l ) | to_uchar( r ) );
+    }
+    JASEL_MUTABLE_CONSTEXPR byte& operator&=(byte& l, byte r) noexcept
+    {
+      return l = to_byte( to_uchar( l ) & to_uchar( r ) );
+    }
+    constexpr byte operator&(byte l, byte r) noexcept
+    {
+      return to_byte( to_uchar( l ) & to_uchar( r ) );
+    }
+    JASEL_MUTABLE_CONSTEXPR byte& operator^=(byte& l, byte r) noexcept
+    {
+      return l = to_byte( to_uchar( l ) ^ to_uchar( r ) );
+    }
+    constexpr byte operator^(byte l, byte r) noexcept
+    {
+      return to_byte( to_uchar( l ) ^ to_uchar( r ) );
+    }
+    constexpr byte operator~(byte b) noexcept
+    {
+      return to_byte( ~ to_uchar( b ) );
+    }
+#endif
+    static_assert(sizeof(byte) == 1, "byte must have size 1");
+}
+}
+}
+
+#endif // header
---
+++ test/strong/byte_pass.cpp
@@ -0,0 +1,122 @@
+// Copyright (C) 2017 Vicente J. Botet Escriba
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// <experimental/strong_id.hpp>
+
+#include <experimental/byte.hpp>
+#include <sstream>
+
+#include <boost/detail/lightweight_test.hpp>
+
+namespace stdex = std::experimental;
+
+static_assert(std::is_pod<stdex::byte>::value, "byte is not a POD");
+
+int main()
+{
+  {
+#if defined JASEL_HAVE_ENUM_CLASS_CONSTRUCTION_FROM_UNDERLYING_TYPE
+    stdex::byte b = static_cast<stdex::byte>( 4 );
+    BOOST_TEST( static_cast<unsigned char>(b) == 4 );
+    BOOST_TEST( stdex::to_integer<int>( b ) == 4 );
+#endif
+  }
+  {
+#if defined JASEL_HAVE_ENUM_CLASS_CONSTRUCTION_FROM_UNDERLYING_TYPE
+    stdex::byte b = stdex::byte( 4 );
+    BOOST_TEST( stdex::to_integer<int>( b ) == 4 );
+#endif
+  }
+  {
+    stdex::byte b = stdex::to_byte( 4 );
+    BOOST_TEST( stdex::to_integer<int>( b ) == 4 );
+  }
+  {
+    stdex::byte a = stdex::to_byte( 3 );
+    stdex::byte b = stdex::to_byte( 7 );
+
+    BOOST_TEST(     a == a );
+    BOOST_TEST(     a != b );
+
+    BOOST_TEST(     a <  b );
+    BOOST_TEST(     a <= a );
+    BOOST_TEST(     a <= b );
+
+    BOOST_TEST(     b >  a );
+    BOOST_TEST(     b >= a );
+    BOOST_TEST(     b >= b );
+
+    BOOST_TEST( ! (a == b) );
+    BOOST_TEST( ! (a != a) );
+    BOOST_TEST( ! (b <  a) );
+    BOOST_TEST( ! (a > b) );
+  }
+  {
+    stdex::byte const b = stdex::to_byte( 0xa5 );
+
+    BOOST_TEST( ( b | b ) == b );
+    BOOST_TEST( ( b | stdex::to_byte( 0x00 ) ) == b );
+    BOOST_TEST( ( b | stdex::to_byte( 0xff ) ) == stdex::to_byte( 0xff ) );
+  }
+  {
+    stdex::byte const b = stdex::to_byte( 0xa5 );
+
+    BOOST_TEST( ( b & b ) == b );
+    BOOST_TEST( ( b & stdex::to_byte( 0xff ) ) == b );
+    BOOST_TEST( ( b & stdex::to_byte( 0x00 ) ) == stdex::to_byte( 0x00 ) );
+  }
+  {
+    stdex::byte const b = stdex::to_byte( 0xa5 );
+
+    BOOST_TEST( ( b ^ b ) == stdex::to_byte( 0 ) );
+    BOOST_TEST( ( b ^ stdex::to_byte( 0x00 ) ) ==  b );
+    BOOST_TEST( ( b ^ stdex::to_byte( 0xff ) ) == ~b );
+  }
+  {
+    stdex::byte const b_org = stdex::to_byte( 0xa5 );
+    stdex::byte       b     = b_org;
+
+    BOOST_TEST( ( b |= b               ) == b_org           );
+    BOOST_TEST( ( b |= stdex::to_byte( 0x00 ) ) == b_org           );
+    BOOST_TEST( ( b |= stdex::to_byte( 0xff ) ) == stdex::to_byte( 0xff ) );
+  }
+  {
+    stdex::byte const b_org = stdex::to_byte( 0xa5 );
+    stdex::byte       b     = b_org;
+
+    BOOST_TEST( ( b &= b               ) == b_org           );
+    BOOST_TEST( ( b &= stdex::to_byte( 0xff ) ) == b_org           );
+    BOOST_TEST( ( b &= stdex::to_byte( 0x00 ) ) == stdex::to_byte( 0x00 ) );
+  }
+  {
+    stdex::byte const b_org = stdex::to_byte( 0xa5 );
+    stdex::byte       b     = b_org;
+
+    BOOST_TEST( ( b ^= b               ) == stdex::to_byte( 0 ) );
+    b ^= stdex::to_byte( 0x00 );
+  }
+  {
+    stdex::byte const b_org = stdex::to_byte( 0xa5 );
+    stdex::byte       b     = b_org;
+
+    b ^= stdex::to_byte( 0x00 );
+    BOOST_TEST_EQ( stdex::to_integer<>( b ),  stdex::to_integer<>(b_org)       );
+  }
+  {
+    stdex::byte const b_org = stdex::to_byte( 0xa5 );
+    stdex::byte       b     = b_org;
+
+    b ^= stdex::to_byte( 0xff );
+    BOOST_TEST_EQ( stdex::to_integer<>( b ),  stdex::to_integer<>(~b_org) );
+  }
+  {
+    stdex::byte const b = stdex::to_byte( 0xa5 );
+
+    BOOST_TEST( ( b << 3 ) == stdex::to_byte( to_uchar( b ) << 3 ) );
+
+  }
+  return ::boost::report_errors();
+}
+
