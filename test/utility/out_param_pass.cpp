556111af615e5c612c86524a44cf04d2d2782875
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-04-15 18:06:59 +0200

    update test with valid content.

--- test/utility/out_param_pass.cpp
+++ test/utility/out_param_pass.cpp
@@ -13,68 +13,19 @@
 
 namespace stdex = std::experimental;
 
-struct Big1 { std::string data; Big1(Big1&&) = delete; Big1() = default; };
-struct Big2 { std::string data; Big2(Big2&&) = delete; Big2() = default; };
-
-struct Processor
+void assign(stdex::out_param<std::string&> obj, std::string const& value)
 {
-  Big1 const& _big1;
-  Big2 const& _big2;
-  explicit Processor(stdex::lvalue_ref<const Big1> b1, stdex::lvalue_ref<const Big2> b2) : _big1(b1), _big2(b2) {}
-  bool process() { return _big1.data == _big2.data; } // this triggers UB on dangling references
-};
-
-//# if (defined __GNUC__) && (!defined __clang__) && (__GNUC__ < 4 || __GNUC__ == 4 && __GNUC_MINOR__ < 8 || __GNUC__ == 4 && __GNUC_MINOR__ == 8 && __GNUC_PATCHLEVEL__ < 1)
-//
-//void static_test_proxy_ref(){}
-//
-//# else
-
-struct ProxyReference
-{
-  int i;
-  operator int&() & { return i; }
-  operator const int&() const & { return i; }
-  operator const int&() const && = delete;
-};
-
-void static_test_proxy_ref()
-{
-  static_assert(std::is_convertible<ProxyReference&, stdex::lvalue_ref<int>>::value, "test failure");
-  static_assert(std::is_convertible<ProxyReference const&, stdex::lvalue_ref<const int>>::value, "test failure");
-  static_assert(!std::is_convertible<ProxyReference&&, stdex::lvalue_ref<int>>::value, "test failure");
-  static_assert(!std::is_convertible<ProxyReference&&, stdex::lvalue_ref<const int>>::value, "test failure");
-  static_assert(!std::is_convertible<ProxyReference const&&, stdex::lvalue_ref<const int>>::value, "test failure");
-}
-//# endif
-
-void static_lvalue_ref_conversions_succeed()
-{
-  static_assert(std::is_constructible<Processor, const Big1&, const Big2&>::value, "test failure");
-  static_assert(std::is_constructible<Processor, std::reference_wrapper<const Big1>&, std::reference_wrapper<const Big2>>::value, "test failure");
-}
-
-void static_rvalue_ref_conversions_fail()
-{
-  static_assert(!std::is_constructible<Processor, Big1, const Big2&>::value, "test failure");
-  static_assert(!std::is_constructible<Processor, const Big1&, const Big2&&>::value, "test failure");
-}
-
-void demonstrate_lvalue_ref()
-{
-  const Big1 b1 {}; // just shows usage
-  const Big2 b2 {};
-  Processor p {b1, b2};
-  (void)p.process();
+  obj.get() = value;
 }
 
 
 int main()
 {
-  static_test_proxy_ref();
-  static_lvalue_ref_conversions_succeed();
-  static_rvalue_ref_conversions_fail();
-  demonstrate_lvalue_ref();
+  {
+    std::string s;
+    assign(stdex::out(s), "value");
+    assert (s == "value");
+  }
 
   return ::boost::report_errors();
 }
