c5f87cdd3f3bbae7abad88befca1ddac26a17d00
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-04-15 11:19:18 +0200

    fix conversion to optional<cons T> from optional_ref<const T>.

--- include/experimental/fundamental/v3/pod/optionals/optionals.hpp
+++ include/experimental/fundamental/v3/pod/optionals/optionals.hpp
@@ -24,6 +24,8 @@
 //    T -> const T or
 //    U* is convertible to T* and sizeof(T) == sizeof(U) ?
 // * Should the type of the mask be a parameter?
+// * Should we define comparisons between optional_ref<I,T> and T?
+//    or add a just<T> (like unexpected<E>) that compares to optional_ref<I,T>?
 
 // Related classes
 // * add a optionals_view that takes a reference to an instance, and it is parameterized by the mask data member pointer and the tuple of each of the value data member pointer
@@ -32,8 +34,8 @@
 //  template <class C, class MaskT, MaskT C::*Mask, class ...T, T C::*Value...>
 //  class optionals_view< member<class C, class MaskT, MaskT C::*Mask>, member<class Mask, class C, class T, T C::*Value>...>;
 //  This class could be used on 3pp legacy C-structs that use a mask for the presence of several fields.
-// What all these classes have in common
-// If we make abstraction of the storage, all these classes will define almost the same interface.
+// What these two classes have in common?
+// If we make abstraction of the storage, both classes will define almost the same interface.
 //
 
 namespace std
@@ -44,7 +46,7 @@ inline namespace fundamental_v3
 {
 namespace pod
 {
-  // fixme: this ca be shared
+  // fixme: this can be shared. Move it to meta
   // this works only if unique
 namespace detail
 {
@@ -202,6 +204,8 @@ public:
     friend constexpr bool operator >= (nullopt_t x, const optional_ref & y) noexcept
     { return x >= optional<T>(y); }
 
+    // fixme: should we define this comparisons?
+    // of add a just<T> that compares to optional_ref<I,T>
     friend constexpr bool operator == (const optional_ref & x, const T&  y) noexcept
     { return optional<T>(x) == y; }
     friend constexpr bool operator != (const optional_ref & x, const T&  y) noexcept
@@ -230,7 +234,6 @@ public:
 
     void swap(optional_ref& rhs) noexcept
     {
-            std::swap(index, rhs.index);
             std::swap(mask, rhs.mask);
             std::swap(ref, rhs.ref);
     }
@@ -294,7 +297,7 @@ public:
         return v;
     }
 
-    explicit JASEL_CXX14_CONSTEXPR operator optional<T>() const
+    explicit JASEL_CXX14_CONSTEXPR operator optional<const T>() const
     {
       if (has_value())
         return *get();
@@ -302,73 +305,72 @@ public:
     }
 
     friend constexpr bool operator == (const optional_ref & x, const optional_ref & y) noexcept
-    { return optional<T>(x) == optional<T>(y); }
+    { return optional<const T>(x) == optional<const T>(y); }
     friend constexpr bool operator != (const optional_ref & x, const optional_ref & y) noexcept
-    { return optional<T>(x) != optional<T>(y); }
+    { return optional<const T>(x) != optional<const T>(y); }
     friend constexpr bool operator < (const optional_ref & x, const optional_ref & y) noexcept
-    { return optional<T>(x) < optional<T>(y); }
+    { return optional<const T>(x) < optional<const T>(y); }
     friend constexpr bool operator <= (const optional_ref & x, const optional_ref & y) noexcept
-    { return optional<T>(x) <= optional<T>(y); }
+    { return optional<const T>(x) <= optional<const T>(y); }
     friend constexpr bool operator > (const optional_ref & x, const optional_ref & y) noexcept
-    { return optional<T>(x) > optional<T>(y); }
+    { return optional<const T>(x) > optional<const T>(y); }
     friend constexpr bool operator >= (const optional_ref & x, const optional_ref & y) noexcept
-    { return optional<T>(x) >= optional<T>(y); }
+    { return optional<const T>(x) >= optional<const T>(y); }
 
     friend constexpr bool operator == (const optional_ref & x, nullopt_t y) noexcept
-    { return optional<T>(x) == y; }
+    { return optional<const T>(x) == y; }
     friend constexpr bool operator != (const optional_ref & x, nullopt_t y) noexcept
-    { return optional<T>(x) != y; }
+    { return optional<const T>(x) != y; }
     friend constexpr bool operator < (const optional_ref & x, nullopt_t y) noexcept
-    { return optional<T>(x) < y; }
+    { return optional<const T>(x) < y; }
     friend constexpr bool operator <= (const optional_ref & x, nullopt_t y) noexcept
-    { return optional<T>(x) <= y; }
+    { return optional<const T>(x) <= y; }
     friend constexpr bool operator > (const optional_ref & x, nullopt_t y) noexcept
-    { return optional<T>(x) > y; }
+    { return optional<const T>(x) > y; }
     friend constexpr bool operator >= (const optional_ref & x, nullopt_t y) noexcept
-    { return optional<T>(x) >= y; }
+    { return optional<const T>(x) >= y; }
 
     friend constexpr bool operator == (nullopt_t x, const optional_ref & y) noexcept
-    { return x == optional<T>(y); }
+    { return x == optional<const T>(y); }
     friend constexpr bool operator != (nullopt_t x, const optional_ref & y) noexcept
-    { return x != optional<T>(y); }
+    { return x != optional<const T>(y); }
     friend constexpr bool operator < (nullopt_t x, const optional_ref & y) noexcept
-    { return x < optional<T>(y); }
+    { return x < optional<const T>(y); }
     friend constexpr bool operator <= (nullopt_t x, const optional_ref & y) noexcept
-    { return x <= optional<T>(y); }
+    { return x <= optional<const T>(y); }
     friend constexpr bool operator > (nullopt_t x, const optional_ref & y) noexcept
-    { return x > optional<T>(y); }
+    { return x > optional<const T>(y); }
     friend constexpr bool operator >= (nullopt_t x, const optional_ref & y) noexcept
-    { return x >= optional<T>(y); }
+    { return x >= optional<const T>(y); }
 
     friend constexpr bool operator == (const optional_ref & x, const T&  y) noexcept
-    { return optional<T>(x) == y; }
+    { return optional<const T>(x) == y; }
     friend constexpr bool operator != (const optional_ref & x, const T&  y) noexcept
-    { return optional<T>(x) != y; }
+    { return optional<const T>(x) != y; }
     friend constexpr bool operator < (const optional_ref & x, const T&  y) noexcept
-    { return optional<T>(x) < y; }
+    { return optional<const T>(x) < y; }
     friend constexpr bool operator <= (const optional_ref & x, const T&  y) noexcept
-    { return optional<T>(x) <= y; }
+    { return optional<const T>(x) <= y; }
     friend constexpr bool operator > (const optional_ref & x, const T&  y) noexcept
-    { return optional<T>(x) > y; }
+    { return optional<const T>(x) > y; }
     friend constexpr bool operator >= (const optional_ref & x, const T&  y) noexcept
-    { return optional<T>(x) >= y; }
+    { return optional<const T>(x) >= y; }
 
     friend constexpr bool operator == (const T&  x, const optional_ref & y) noexcept
-    { return x == optional<T>(y); }
+    { return x == optional<const T>(y); }
     friend constexpr bool operator != (const T&  x, const optional_ref & y) noexcept
-    { return x != optional<T>(y); }
+    { return x != optional<const T>(y); }
     friend constexpr bool operator < (const T&  x, const optional_ref & y) noexcept
-    { return x < optional<T>(y); }
+    { return x < optional<const T>(y); }
     friend constexpr bool operator <= (const T&  x, const optional_ref & y) noexcept
-    { return x <= optional<T>(y); }
+    { return x <= optional<const T>(y); }
     friend constexpr bool operator > (const T&  x, const optional_ref & y) noexcept
-    { return x > optional<T>(y); }
+    { return x > optional<const T>(y); }
     friend constexpr bool operator >= (const T&  x, const optional_ref & y) noexcept
-    { return x >= optional<T>(y); }
+    { return x >= optional<const T>(y); }
 
     void swap(optional_ref& rhs) noexcept
     {
-            std::swap(index, rhs.index);
             std::swap(mask, rhs.mask);
             std::swap(ref, rhs.ref);
     }
@@ -401,19 +403,30 @@ struct optional_ref_fact {
 };
 }
 
-/**
-optionals<Ts..> behaves similarly to tuple<optional<Ts>...>
-get<T> returns a optional_ref<T> that behaves as a proxy.
-*/
+
+/// `optionals<Ts..>` behaves similarly to `tuple<optional<Ts>...>` when the `Ts` are `POD`.
+/// `get<T>` returns an `optional_ref<T>` that behaves as a proxy.
+/// `optionals<Ts..>` is a POD
+/// However the interface is similar to `variant<Ts...>`, `get`/`get_if` but the reason for the presence are different.
+/// In the case of variant is because only one of the element are there
+/// In the case of optionals is because each element can be there or not.
+
+
+/// @tparam Ts parameter pack of PODs
 template <class ...Ts>
 class optionals
 {
     using Types = tuple<Ts...>;
 public:
-    //Requires all Ts are POD.
-    // This is a POD
+    //todo add requires all Ts are POD.
+
+    /// @par Effects uninitialized constructor
     constexpr optionals() = default;
 
+    //! You should use
+    //! pod::optionals<Ts..> opts;
+    //! opts.construct();
+    // @par Effects Constructs the object that does not contain a value.
     void construct()  noexcept
     {
       mask = false;
@@ -423,6 +436,32 @@ public:
     //optionals(optionals &&) = default;
     //optionals& operator=(optionals const&) = default;
     //optionals& operator=(optionals &&) = default;
+
+    //! You could use
+    //! opts.copy_assign(opt2);
+    //! which will copy the values type only if m_present.
+    //! Note that the defaul copy and assignment will copy all the bytes.
+    //! fixme: Which one is more efficient
+    void copy_assign(optionals const& other) noexcept
+    {
+      if (other.mask != 0)
+      {
+        // todo copy assign only the present ones
+        tpl = other.tpl;
+      }
+      mask =  other.mask;
+    }
+
+    // fixme, as POD have trivial copy and move and move is equivalent to copy, has this a sense
+    void move_assign(optionals && other) noexcept
+    {
+      if (other.mask != 0)
+      {
+        // todo move assign only the present ones
+        tpl = move(other.tpl);
+      }
+      mask =  other.mask;
+    }
 
     template <size_t I>
     constexpr bool has_value() const noexcept
@@ -436,29 +475,32 @@ public:
         return has_value<detail::index<Types, T>::value>();
     }
 
-    /// @pre has_value<I>
+    /// @par Pre-condition has_value<I>
     template <size_t I>
     JASEL_CXX14_CONSTEXPR typename tuple_element<I, Types>::type& get_ref()
     {
         return std::get<I>(tpl);
     }
+    /// @par Pre-condition has_value<I>
     template <size_t I>
     constexpr typename tuple_element<I, Types>::type const& get_ref() const
     {
         return std::get<I>(tpl);
     }
-    /// @pre has_value<T>
+    /// @par Pre-condition has_value<T>
     template <class T>
     constexpr T & get_ref()
     {
         return get_ref<detail::index<Types, T>::value>();
     }
+    /// @par Pre-condition has_value<T>
     template <class T>
     constexpr T const& get_ref() const
     {
         return get_ref<detail::index<Types, T>::value>();
     }
 
+    /// @returns `nullptr` if `! has_value<I>()`, otherwise the address of the associated storage
     template <size_t I>
     JASEL_CXX14_CONSTEXPR auto get_if() noexcept -> decltype(&get_ref<I>())
     {
@@ -466,6 +508,7 @@ public:
         return &get_ref<I>();
       return nullptr;
     }
+    /// @returns `nullptr` if `! has_value<I>()`, otherwise the address of the associated storage
     template <size_t I>
     JASEL_CXX14_CONSTEXPR auto get_if() const noexcept -> decltype(&get_ref<I>())
     {
@@ -473,23 +516,27 @@ public:
         return &get_ref<I>();
       return nullptr;
     }
+    /// @returns `nullptr` if `! has_value<T>()`, otherwise the address of the associated storage
     template <class T>
     JASEL_CXX14_CONSTEXPR T* get_if() noexcept
     {
       return get_if<detail::index<Types, T>::value>();
     }
+    /// @returns `nullptr` if `! has_value<T>()`, otherwise the address of the associated storage
     template <class T>
     constexpr T const* get_if() const noexcept
     {
       return get_if<detail::index<Types, T>::value>();
     }
 
+    /// @returns A optional_ref instance that behaves an optional except that the storage is collocated in this optional.
     template <size_t I>
     JASEL_CXX14_CONSTEXPR optional_ref<I, typename tuple_element<I, Types>::type> get_opt() noexcept
     {
         using T = typename tuple_element<I, Types>::type;
         return detail::optional_ref_fact<I, T>::make(mask, get_ref<I>());
     }
+    /// @returns A const optional_ref instance that behaves an optional except that the storage is collocated in this optional.
     template <size_t I>
     constexpr optional_ref<I, const typename tuple_element<I, Types>::type> get_opt() const noexcept
     {
@@ -526,14 +573,6 @@ public:
 private:
     size_t mask;
     Types tpl;
-
-    template <size_t I>
-    void set_mask()
-    {
-        mask |= (1 << I);
-    }
-
-
 };
 
 // Optionals should be a model of product_type `optional<Ts>...`. However `get` returns a proxy
