a3b42ea89baf0ac23bda9cc06be400ddc75a2d61
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-04-29 19:03:23 +0200

    Added missing file.

---
+++ Version.cmake
@@ -0,0 +1,5 @@
+# To update the jasel version, from a *CLEAN* working directory, update the version numbers below.
+# This makefile will generate a new version.hpp, *AMEND THE MOST RECENT COMMIT*, and git-tag the commit.
+set(JASEL_MAJOR 0)
+set(JASEL_MINOR 3)
+set(JASEL_PATCHLEVEL 1)
