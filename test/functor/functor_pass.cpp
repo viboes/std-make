9f8b2a6be2a6840e32643157f484e711ec57b14e
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-05-13 11:21:38 +0200

    add functor test for pointer.

--- test/functor/functor_pass.cpp
+++ test/functor/functor_pass.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2014-2015 Vicente J. Botet Escriba
+// Copyright (C) 2014-2017 Vicente J. Botet Escriba
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -6,8 +6,12 @@
 // <experimental/functor.hpp>
 
 #include <experimental/functor.hpp>
+#include <experimental/nullable.hpp>
 #include <boost/detail/lightweight_test.hpp>
 
+int twice(int i) {
+  return 2*i;
+}
 
 int main()
 {
@@ -15,5 +19,47 @@ int main()
 
   static_assert(! stde::is_functor<int>::value, "");
 
+  static_assert(stde::is_nullable<int*>::value, "ERROR");
+  static_assert(stde::is_type_constructible<int*>::value, "ERROR");
+  static_assert(std::is_base_of<stde::functor::tag, stde::functor::traits<int*>> ::value, "ERROR");
+  static_assert(stde::is_functor<int*>::value, "ERROR");
+
+  {
+    int* x = stde::none<int*>();
+    BOOST_TEST(! x);
+    BOOST_TEST(! stde::has_value(x));
+    BOOST_TEST(x == stde::none());
+    BOOST_TEST(stde::none() == x);
+    BOOST_TEST(x == stde::none<int*>());
+
+    int* y = stde::functor::transform(x, twice);
+    BOOST_TEST(! stde::has_value(y));
+  }
+  {
+    int v=1;
+    int* x = stde::make<int*>(v);
+    BOOST_TEST(*x == 1);
+    BOOST_TEST(x != stde::none());
+    BOOST_TEST(stde::deref(x) == 1);
+    int* y = stde::functor::transform(x, twice);
+    BOOST_TEST(stde::deref(y) == 2);
+    delete x; // don't use T* when you must manage memory. Use instead unique_ptr
+    delete y;
+    x = stde::make<int*>(2);
+    BOOST_TEST(stde::deref(x) == 2);
+    delete x;
+  }
+  {
+    int v=1;
+    const int* x = stde::make<int*>(v);
+    BOOST_TEST(*x == 1);
+    BOOST_TEST(x != stde::none());
+    BOOST_TEST(stde::deref(x) == 1);
+
+    int* y = stde::functor::transform(x, twice);
+    BOOST_TEST(stde::deref(y) == 2);
+    delete x;
+    delete y;
+  }
   return ::boost::report_errors();
 }
