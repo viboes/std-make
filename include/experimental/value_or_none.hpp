69189ff0775ffff02f5cb4d566113f0f7f754cc0
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-10-03 23:59:54 +0200

    fix typo.

---
+++ include/experimental/value_or_none.hpp
@@ -0,0 +1,47 @@
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// (C) Copyright 2017 Vicente J. Botet Escriba
+
+#ifndef JASEL_EXPERIMENTAL_VALUE_OR_NONE_HPP
+#define JASEL_EXPERIMENTAL_VALUE_OR_NONE_HPP
+
+
+// Nullable customizations
+// as functor
+#include <experimental/fundamental/v3/value_or_none/transform.hpp>
+
+// as applicative
+#include <experimental/fundamental/v3/value_or_none/ap.hpp>
+
+// as monad
+#include <experimental/fundamental/v3/value_or_none/bind.hpp>
+
+// as monad_error
+#include <experimental/fundamental/v3/value_or_none/monad_error.hpp>
+
+// as sum_type
+#include <experimental/fundamental/v3/value_or_none/visit.hpp>
+
+// as foldable
+//#include <experimental/fundamental/v3/value_or_none/fold.hpp>
+
+// as traversable
+//#include <experimental/fundamental/v3/value_or_none/traverse.hpp>
+//#include <experimental/fundamental/v3/value_or_none/for_each.hpp>
+
+// ValueOrNone algos
+#include <experimental/fundamental/v3/value_or_none/value_or.hpp>
+#include <experimental/fundamental/v3/value_or_none/error_or.hpp>
+#include <experimental/fundamental/v3/value_or_none/has_error.hpp>
+#include <experimental/fundamental/v3/value_or_none/apply_or.hpp>
+#include <experimental/fundamental/v3/value_or_none/resolve.hpp>
+
+// Nullable core
+#include <experimental/fundamental/v3/value_or_none/value_or_none.hpp>
+
+// TypeConstructible
+#include <experimental/type_constructible.hpp>
+
+#endif // header
