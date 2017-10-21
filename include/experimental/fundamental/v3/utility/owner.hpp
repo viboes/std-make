2ffa9adb246bb24c06a35499796a81699a6bd6ce
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-10-21 16:34:03 +0200

    Added owner.

---
+++ include/experimental/fundamental/v3/utility/owner.hpp
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2017.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file // LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef JASEL_FUNDAMENTAL_V3_UTILITY_OWNER_HPP
+#define JASEL_FUNDAMENTAL_V3_UTILITY_OWNER_HPP
+
+#include <stdexcept>
+#include <experimental/fundamental/v2/config.hpp>
+
+namespace std
+{
+namespace experimental
+{
+inline namespace fundamental_v3
+{
+
+  template< class T > using owner = T;
+
+}
+}
+}
+#endif // header
