8adfe1ad029d4ded301e9a6c789a9ea9f2513dd8
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2018-03-02 12:14:38 +0100

    result: extract sucess/failure tests.

---
+++ test/result/success_failure_pass.cpp
@@ -0,0 +1,100 @@
+
+// Use, modification and distribution are subject to the
+// Boost Software License, Version 1.0.
+//(See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Copyright Vicente J. Botet Escriba 2018.
+
+#include <boost/detail/lightweight_test.hpp>
+#if __cplusplus >= 201402L && defined __clang__
+
+#include <experimental/result.hpp>
+#include <memory>
+#include <vector>
+#include <string>
+
+namespace stdex = std::experimental;
+
+template <class A>
+auto g(A&& x)
+{
+    return stdex::success<std::unique_ptr<int>> (std::forward<A>(x));
+}
+int main()
+{
+    {
+        stdex::success<std::pair<int, short>> x =  stdex::make_success(std::make_pair(1,2));
+        BOOST_TEST(x.value.first == 1);
+        BOOST_TEST(x.value.second == 2);
+    }
+    {
+        stdex::success<std::pair<int, short>> x {std::in_place, 1,2};
+        BOOST_TEST(x.value.first == 1);
+        BOOST_TEST(x.value.second == 2);
+    }
+    {
+        stdex::success<short> x =  stdex::make_success(1);
+        BOOST_TEST(x.value == 1);
+    }
+    {
+        stdex::failure<short> x =  stdex::make_failure(1);
+        BOOST_TEST(x.value == 1);
+    }
+    {
+        auto x =  stdex::make_success(std::string(""));
+        static_assert(! std::is_same<decltype(x),std::string>::value, "ERROR");
+        BOOST_TEST(x.value == "");
+    }
+    {
+        stdex::success<std::string> x =  stdex::make_success(std::string(""));
+        auto y = x;
+        BOOST_TEST(y.value == "");
+    }
+    {
+        stdex::success<std::string> x =  stdex::make_success("");
+        auto y = x;
+        BOOST_TEST(y.value == "");
+    }
+    {
+        auto x = stdex::make_success(std::string("aaa"));
+        auto y = std::move(x);
+        BOOST_TEST(x.value == "");
+        BOOST_TEST(y.value == "aaa");
+    }
+    {
+        auto x =  stdex::make_success(std::make_unique<int>(1));
+        static_assert(! std::is_same<decltype(x),std::unique_ptr<int>>::value, "ERROR");
+        BOOST_TEST(*x.value == 1);
+    }
+    {
+        auto x =  stdex::make_success(std::make_unique<int>(1));
+        auto y = std::move(x);
+        BOOST_TEST(x.value == nullptr);
+        BOOST_TEST(*y.value == 1);
+    }
+    {
+        auto x =  std::make_unique<int>(1);
+        auto y = stdex::make_success(std::move(x));
+        BOOST_TEST(*y.value == 1);
+    }
+    {
+        auto x = stdex::make_success(std::make_unique<int>(1));
+        auto y = std::move(x);
+        BOOST_TEST(*y.value == 1);
+    }
+    {
+        auto x = stdex::make_success(std::make_unique<int>(1));
+        auto y = g(std::move(x));
+        BOOST_TEST(*y.value == 1);
+    }
+
+    return ::boost::report_errors();
+}
+
+#else
+int main()
+{
+    return ::boost::report_errors();
+}
+#endif
--- test/result/trivial_result_pass.cpp
+++ test/result/trivial_result_pass.cpp
@@ -31,68 +31,9 @@ stdex::result<int, short> f(int v, int w) {
         return stdex::make_failure(-1);
 }
 
-template <class A>
-auto g(A&& x)
-{
-    return stdex::success<std::unique_ptr<int>> (std::forward<A>(x));
-}
+
 int main()
 {
-    {
-        stdex::success<std::pair<int, short>> x =  stdex::make_success(std::make_pair(1,2));
-        BOOST_TEST(x.value.first == 1);
-        BOOST_TEST(x.value.second == 2);
-    }
-    {
-        stdex::success<std::pair<int, short>> x {std::in_place, 1,2};
-        BOOST_TEST(x.value.first == 1);
-        BOOST_TEST(x.value.second == 2);
-    }
-    {
-        stdex::success<short> x =  stdex::make_success(1);
-        BOOST_TEST(x.value == 1);
-    }
-    {
-        stdex::failure<short> x =  stdex::make_failure(1);
-        BOOST_TEST(x.value == 1);
-    }
-    {
-        auto x =  stdex::make_success(std::string(""));
-        static_assert(! std::is_same<decltype(x),std::string>::value, "ERROR");
-        BOOST_TEST(x.value == "");
-    }
-    {
-        stdex::success<std::string> x =  stdex::make_success(std::string(""));
-        auto y = x;
-        BOOST_TEST(y.value == "");
-    }
-    {
-        stdex::success<std::string> x =  stdex::make_success("");
-        auto y = x;
-        BOOST_TEST(y.value == "");
-    }
-    {
-        auto x = stdex::make_success(std::string("aaa"));
-        auto y = std::move(x);
-        BOOST_TEST(x.value == "");
-        BOOST_TEST(y.value == "aaa");
-    }
-    {
-        auto x =  stdex::make_success(std::make_unique<int>(1));
-        static_assert(! std::is_same<decltype(x),std::unique_ptr<int>>::value, "ERROR");
-        BOOST_TEST(*x.value == 1);
-    }
-    {
-        auto x =  stdex::make_success(std::make_unique<int>(1));
-        auto y = std::move(x);
-        BOOST_TEST(x.value == nullptr);
-        BOOST_TEST(*y.value == 1);
-    }
-    {
-        auto x =  std::make_unique<int>(1);
-        auto y = stdex::make_success(std::move(x));
-        BOOST_TEST(*y.value == 1);
-    }
     {
         stdex::result<int, short> res = stdex::make_success(1);
         BOOST_TEST(res.has_value());
@@ -151,16 +92,6 @@ int main()
         stdex::result<std::unique_ptr<int>, short> res = stdex::make_success(std::make_unique<int>(1));
         BOOST_TEST(res.has_value());
         BOOST_TEST(*res.value() == 1);
-    }
-    {
-        auto x = stdex::make_success(std::make_unique<int>(1));
-        auto y = std::move(x);
-        BOOST_TEST(*y.value == 1);
-    }
-    {
-        auto x = stdex::make_success(std::make_unique<int>(1));
-        auto y = g(std::move(x));
-        BOOST_TEST(*y.value == 1);
     }
     {
         stdex::result<std::unique_ptr<int>, short> res = stdex::make_success(std::make_unique<int>(1));
