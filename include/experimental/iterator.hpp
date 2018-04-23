4573dcb586e14cf8436410568082d4c4b6d3877c
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2018-04-23 10:48:02 +0200

    ../include/experimental/fundamental/v3/iterator/categoy_traits.hpp
    iterator: Added category traits.

---
+++ include/experimental/iterator.hpp
@@ -0,0 +1,13 @@
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// (C) Copyright 2018 Vicente J. Botet Escriba
+
+#ifndef JASEL_EXPERIMENTAL_ITERATOR_HPP
+#define JASEL_EXPERIMENTAL_ITERATOR_HPP
+
+#include <experimental/fundamentail/v3/iterator/category_traits.hpp>
+
+
+#endif // header
