c240c95f0a745467d7e79b7c6b1464756e753544
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2018-04-23 14:21:53 +0200

    functional: prepare functions to be included.

--- include/experimental/functional.hpp
+++ include/experimental/functional.hpp
@@ -2,14 +2,21 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-// (C) Copyright 2016 Vicente J. Botet Escriba
+// (C) Copyright 2016-2018 Vicente J. Botet Escriba
 
 #ifndef JASEL_EXPERIMENTAL_FUNCTIONAL_HPP
 #define JASEL_EXPERIMENTAL_FUNCTIONAL_HPP
 
 #include <experimental/fundamental/v3/functional/always.hpp>
+//#include <experimental/fundamental/v3/functional/bind_front.hpp>
+//#include <experimental/fundamental/v3/functional/compose.hpp>
+//#include <experimental/fundamental/v3/functional/first_overload.hpp>
 #include <experimental/fundamental/v3/functional/identity.hpp>
+//#include <experimental/fundamental/v3/functional/invoke.hpp>
+//#include <experimental/fundamental/v3/functional/not_fn.hpp>
 #include <experimental/fundamental/v3/functional/overload.hpp>
+//#include <experimental/fundamental/v3/functional/fix.hpp>
+
 #include <functional>
 
 #endif // header
