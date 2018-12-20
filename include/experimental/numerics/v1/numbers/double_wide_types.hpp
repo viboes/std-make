36d9776cad54adb4c0005ad6f539e706277b5fe4
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2018-12-20 16:12:05 +0100

    make use of narrow to define signed conversions and test them.

--- include/experimental/numerics/v1/numbers/double_wide_types.hpp
+++ include/experimental/numerics/v1/numbers/double_wide_types.hpp
@@ -2,7 +2,7 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-// Copyright (C) 2017 Vicente J. Botet Escriba
+// Copyright (C) 2017-2018 Vicente J. Botet Escriba
 // Based on http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2017/p0103r1.html
 
 #ifndef JASEL_NUMERIC_V1_NUMBERS_DOUBLE_WIDE_TYPES_HPP
@@ -10,7 +10,10 @@
 
 #include <cstdint>
 #include <climits>
+#include <cassert>
 #include <experimental/type_traits.hpp>
+#include <experimental/contract.hpp>
+#include <experimental/utility.hpp>
 
 namespace std
 {
@@ -20,22 +23,48 @@ namespace numerics
 {
 inline  namespace v1
 {
-  // fixme: Why to redefine it?
+  // Why to redefine it? It can be specialized for UDT
   template < class T >
   struct make_signed : std::make_signed<T> {};
   template <class T>
   using make_signed_t = typename make_signed<T>::type;
 
   template <class T>
-  make_signed_t<T> to_signed(T x) { return static_cast<make_signed_t<T>>(x); }
+  constexpr make_signed_t<T> to_signed_bitcast(T x) noexcept
+  {
+      return static_cast<make_signed_t<T>>(x);
+  }
+  template <class T>
+  constexpr make_signed_t<T> to_signed_cast(T x) noexcept
+  {
+      return narrow_cast<make_signed_t<T>>(x);
+  }
+  template <class T>
+  constexpr make_signed_t<T> to_signed(T x) noexcept
+  {
+      return narrow<make_signed_t<T>>(x);
+  }
 
-  // fixme: Why to redefine it?
+  // Why to redefine it? It can be specialized for UDT
   template < class T >
   struct make_unsigned : std::make_unsigned<T> {};
   template <class T>
   using make_unsigned_t = typename make_unsigned<T>::type;
   template <class T>
-  make_unsigned_t<T> to_unsigned(T x) { return static_cast<make_unsigned_t<T>>(x); }
+  constexpr make_unsigned_t<T> to_unsigned_bitcast(T x) noexcept
+  {
+      return static_cast<make_unsigned_t<T>>(x);
+  }
+  template <class T>
+  constexpr make_unsigned_t<T> to_unsigned_cast(T x) noexcept
+  {
+      return narrow_cast<make_unsigned_t<T>>(x);
+  }
+  template <class T>
+  constexpr make_unsigned_t<T> to_unsigned(T x) noexcept
+  {
+      return narrow<make_unsigned_t<T>>(x);
+  }
 
   template < class T >
   struct make_double_width;
@@ -64,7 +93,10 @@ inline  namespace v1
   template <class T>
   using make_double_width_t = typename make_double_width<T>::type;
   template <class T>
-  double_width<T> to_double_width(T x) { return double_width<T>(x); }
+  constexpr double_width<T> to_double_width(T x) noexcept
+  {
+      return double_width<T>(x);
+  }
 
   template < class T >
   struct make_half_width;
@@ -93,7 +125,10 @@ inline  namespace v1
   template <class T>
   using make_half_width_t = typename make_half_width<T>::type;
   template <class T>
-  half_width<T> to_half_width(T x) { return static_cast<half_width<T>>(x); }
+  constexpr half_width<T> to_half_width(T x) noexcept
+  {
+      return static_cast<half_width<T>>(x);
+  }
 
 
   typedef int single_sword; // this is implementation defined
@@ -102,22 +137,22 @@ inline  namespace v1
   typedef double_width<single_uword> double_uword;
 
   template <typename T>
-  half_width<make_unsigned_t<T>> split_upper( T a )
+  constexpr half_width<make_unsigned_t<T>> split_upper( T a ) noexcept
   {
     static_assert(is_integral<T>::value, "T must be integral");
 
-    return static_cast<half_width<make_unsigned_t<T>>>(to_half_width(to_unsigned(a) >> (CHAR_BIT * sizeof(T) / 2)));
+    return static_cast<half_width<make_unsigned_t<T>>>(to_half_width(to_unsigned_bitcast(a) >> (CHAR_BIT * sizeof(T) / 2)));
   }
   template <typename T>
-  half_width<make_unsigned_t<T>> split_lower( T a )
+  constexpr half_width<make_unsigned_t<T>> split_lower( T a ) noexcept
   {
     static_assert(is_integral<T>::value, "T must be integral");
 
-    return to_half_width(to_unsigned(a));
+    return to_half_width(to_unsigned_bitcast(a));
   }
 
   template <typename T>
-  make_unsigned_t<double_width<T>> wide_unsigned( T high, T low )
+  constexpr make_unsigned_t<double_width<T>> wide_unsigned( T high, T low ) noexcept
   {
     static_assert(is_integral<T>::value, "T must be integral");
     static_assert(is_unsigned<T>::value, "T must be integral");
@@ -125,12 +160,12 @@ inline  namespace v1
     return (to_double_width(high) << (CHAR_BIT * sizeof(T)))  | to_double_width(low);
   }
   template <typename T>
-  make_signed_t<double_width<T>> wide_signed( T high, T low )
+  constexpr make_signed_t<double_width<T>> wide_signed( T high, T low ) noexcept
   {
     static_assert(is_integral<T>::value, "T must be integral");
     static_assert(is_unsigned<T>::value, "T must be integral");
 
-    return to_signed(wide_unsigned(high, low));
+    return to_signed_bitcast(wide_unsigned(high, low));
   }
 
 }
--- test/numerics/numbers/double_wide_types_pass.cpp
+++ test/numerics/numbers/double_wide_types_pass.cpp
@@ -1,10 +1,13 @@
-// Copyright (C) 2017 Vicente J. Botet Escriba
+// Copyright (C) 2017-2018 Vicente J. Botet Escriba
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 // Based on https://github.com/akrzemi1/explicit/blob/master/test/test_explicit.cpp
 
 // <experimental/numerics/v1/numbers/double_wide_types.hpp>
+
+#define JASEL_CONFIG_CONTRACT_LEVEL_MASK 0x111
+
 #include <iostream>
 #include <experimental/numerics/v1/numbers/double_wide_types.hpp>
 #include <string>
@@ -14,10 +17,10 @@
 namespace stdex = std::experimental;
 namespace nmx = std::experimental::numerics;
 
-static_assert(std::is_same<decltype(nmx::to_signed(1)), int>::value, "error");
-static_assert(std::is_same<decltype(nmx::to_signed(1u)), int>::value, "error");
-static_assert(std::is_same<decltype(nmx::to_unsigned(1)), unsigned int>::value, "error");
-static_assert(std::is_same<decltype(nmx::to_unsigned(1u)), unsigned int>::value, "error");
+static_assert(std::is_same<decltype(nmx::to_signed_cast(1)), int>::value, "error");
+static_assert(std::is_same<decltype(nmx::to_signed_cast(1u)), int>::value, "error");
+static_assert(std::is_same<decltype(nmx::to_unsigned_cast(1)), unsigned int>::value, "error");
+static_assert(std::is_same<decltype(nmx::to_unsigned_cast(1u)), unsigned int>::value, "error");
 
 static_assert(std::is_same<decltype(nmx::to_double_width(static_cast<std::int8_t>(1))), std::int16_t>::value, "error");
 static_assert(std::is_same<decltype(nmx::to_double_width(static_cast<std::uint8_t>(1))), std::uint16_t>::value, "error");
@@ -35,28 +38,60 @@ static_assert(std::is_same<decltype(nmx::to_half_width(static_cast<std::uint64_t
 
 int main()
 {
+  //BOOST_TEST(  ! nmx::in_domain<unsigned int>(-1) );
 
-  BOOST_TEST_EQ(nmx::to_signed(1), 1);
-  BOOST_TEST_EQ(nmx::to_signed(1u), 1);
+  {
+      JASEL_CXX14_CONSTEXPR auto r = nmx::to_signed_cast(1);
+      (void)r;
+  }
 
-  BOOST_TEST_EQ(nmx::to_unsigned(1), 1u);
-  BOOST_TEST_EQ(nmx::to_unsigned(1u), 1u);
+
+  BOOST_TEST_EQ(nmx::to_signed_cast(1), 1);
+  BOOST_TEST_EQ(nmx::to_signed_cast(1u), 1);
+
+  {
+      JASEL_CXX14_CONSTEXPR auto r = nmx::to_unsigned_cast(1);
+      (void)r;
+  }
+
+  BOOST_TEST_EQ(nmx::to_unsigned_cast(1), 1u);
+  BOOST_TEST_EQ(nmx::to_unsigned_cast(1u), 1u);
+
+  {
+      JASEL_CXX14_CONSTEXPR auto r = nmx::to_double_width(static_cast<std::int32_t>(1));
+      BOOST_TEST_EQ(r, static_cast<std::int64_t>(1));
+  }
 
   BOOST_TEST_EQ(nmx::to_double_width(static_cast<std::int32_t>(1)), static_cast<std::int64_t>(1));
   BOOST_TEST_EQ(nmx::to_double_width(static_cast<std::uint32_t>(1u)), static_cast<std::uint64_t>(1u));
 
+  {
+      JASEL_CXX14_CONSTEXPR auto r = nmx::to_half_width(static_cast<std::int32_t>(1));
+      BOOST_TEST_EQ(r, static_cast<std::int16_t>(1));
+  }
   BOOST_TEST_EQ(nmx::to_half_width(static_cast<std::int32_t>(1)), static_cast<std::int16_t>(1));
   BOOST_TEST_EQ(nmx::to_half_width(static_cast<std::uint32_t>(1u)), static_cast<std::uint16_t>(1u));
 
+  {
+      JASEL_CXX14_CONSTEXPR auto r = nmx::split_upper(0x00010002);
+      BOOST_TEST_EQ(r, static_cast<unsigned short>(0x0001));
+  }
   BOOST_TEST_EQ(nmx::split_upper(0x00010002), static_cast<unsigned short>(0x0001));
   BOOST_TEST_EQ(nmx::split_lower(0x00010002), static_cast<unsigned short>(0x0002));
 
   BOOST_TEST_EQ(nmx::split_upper(-1), static_cast<unsigned short>(0xffff));
   BOOST_TEST_EQ(nmx::split_lower(-1), static_cast<unsigned short>(0xffff));
 
+  {
+      JASEL_CXX14_CONSTEXPR auto r = nmx::wide_signed(static_cast<unsigned short>(0xffff), static_cast<unsigned short>(0xffff));
+      BOOST_TEST_EQ(r, -1);
+  }
   BOOST_TEST_EQ(nmx::wide_signed(static_cast<unsigned short>(0xffff), static_cast<unsigned short>(0xffff)), -1);
+  {
+      JASEL_CXX14_CONSTEXPR auto r = nmx::wide_unsigned(static_cast<unsigned short>(0xffff), static_cast<unsigned short>(0xffff));
+      BOOST_TEST_EQ(r, static_cast<unsigned>(-1));
+  }
   BOOST_TEST_EQ(nmx::wide_unsigned(static_cast<unsigned short>(0xffff), static_cast<unsigned short>(0xffff)), static_cast<unsigned>(-1));
 
   return ::boost::report_errors();
 }
-
