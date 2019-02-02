583350ebd13486bf3dd4bca873664a4addb94c3f
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2019-02-02 17:52:35 +0100

    added missing file.

---
+++ include/experimental/static_vector.hpp
@@ -0,0 +1,12 @@
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// (C) Copyright 2019 Vicente J. Botet Escriba
+
+#ifndef JASEL_EXPERIMENTAL_STATIC_VECTOR_HPP
+#define JASEL_EXPERIMENTAL_STATIC_VECTOR_HPP
+
+#include <experimental/fundamental/v3/static_vector/static_vector.hpp>
+
+#endif // header
