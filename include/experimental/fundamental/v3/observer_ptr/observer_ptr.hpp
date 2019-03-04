ddf47963110f9b0ae535ee55b17c65b39f485138
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2019-03-04 21:46:28 +0100

    D1495R0

--- include/experimental/fundamental/v3/observer_ptr/observer_ptr.hpp
+++ include/experimental/fundamental/v3/observer_ptr/observer_ptr.hpp
@@ -15,10 +15,17 @@
 #include <experimental/fundamental/v2/config.hpp>
 #include <cstddef>
 #include <functional>
+#include <memory>
 #include <type_traits>
 
-// observer_ptr is a non-owning pointer to a single object (no poinyer arithmetic) providing default construction to nullptr.
+#include <experimental/fundamental/v3/config/requires.hpp>
+
+// observer_ptr is a non-owning pointer to a single object (no pointer arithmetic) providing default construction to nullptr.
 // See https://en.cppreference.com/w/cpp/experimental/observer_ptr
+// See http://wiki.edg.com/pub/Wg21kona2019/LibraryEvolutionWorkingGroup/D1495R0_observer_ptr.pdf
+// 1. Allow implicit conversions from smart and raw pointers
+// 2. Rename/remove release()/detach() (as it does not transfer ownership)
+// 3. Rename observer_ptr => access_ptr, single_ptr
 
 namespace std
 {
@@ -37,18 +44,28 @@ public:
 
 	constexpr observer_ptr() noexcept
 	        : ptr(nullptr) {}
-	constexpr explicit observer_ptr(std::nullptr_t) noexcept
+	constexpr observer_ptr(std::nullptr_t) noexcept
 	        : ptr(nullptr) {}
-	constexpr explicit observer_ptr(T *ptr) noexcept
+	constexpr observer_ptr(T *ptr) noexcept
 	        : ptr(ptr) {}
 
 	observer_ptr(const observer_ptr &other) = default;
 	observer_ptr(observer_ptr &&other)      = default;
 
-	template <class W2>
-	// requires W2* is convertible T*
-	observer_ptr(observer_ptr<W2> other) noexcept
+	template <class W, JASEL_T_REQUIRES(is_convertible<W *, T *>::value)>
+	observer_ptr(observer_ptr<W> other) noexcept
 	        : ptr(other.ptr)
+	{
+	}
+
+	template <class W, class D, JASEL_T_REQUIRES(is_convertible<W *, T *>::value)>
+	constexpr observer_ptr(unique_ptr<W, D> &sp) noexcept
+	        : ptr(sp.get())
+	{
+	}
+	template <class W, JASEL_T_REQUIRES(is_convertible<W *, T *>::value)>
+	constexpr observer_ptr(shared_ptr<W> &sp) noexcept
+	        : ptr(sp.get())
 	{
 	}
 
@@ -65,13 +82,21 @@ public:
 
 	constexpr explicit operator bool() const noexcept { return ptr != nullptr; }
 
-	JASEL_CXX14_CONSTEXPR element_type *release() noexcept
+	JASEL_CXX14_CONSTEXPR element_type *detach() noexcept
 	{
 		element_type *res = ptr;
 		reset();
 		return res;
 	}
-	JASEL_CXX14_CONSTEXPR void reset(element_type *p = nullptr) noexcept { ptr = p; }
+	JASEL_CXX14_CONSTEXPR element_type *release() noexcept
+	{
+		return detach();
+	}
+
+	JASEL_CXX14_CONSTEXPR void reset(element_type *p = nullptr) noexcept
+	{
+		ptr = p;
+	}
 	JASEL_CXX14_CONSTEXPR void swap(observer_ptr &other) noexcept
 	{
 		using std::swap;
@@ -84,7 +109,6 @@ private:
 
 // specialized algorithms:
 // swap:
-
 template <class W>
 JASEL_CXX14_CONSTEXPR void swap(observer_ptr<W> &p1, observer_ptr<W> &p2) noexcept
 {
