3794ab71c4e0d96d3351f981c4874b53896e9056
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2019-03-20 05:26:51 +0100

    Added missing file.

---
+++ include/experimental/static_forward_list.hpp
@@ -0,0 +1,12 @@
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// (C) Copyright 2019 Vicente J. Botet Escriba
+
+#ifndef JASEL_EXPERIMENTAL_STATIC_FORWARD_LIST_HPP
+#define JASEL_EXPERIMENTAL_STATIC_FORWARD_LIST_HPP
+
+#include <experimental/fundamental/v3/static_forward_list/static_forward_list.hpp>
+
+#endif // header
