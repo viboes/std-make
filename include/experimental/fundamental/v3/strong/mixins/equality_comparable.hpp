9068220a4130340cbc7cb0e2de51c47422dc52b3
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2019-05-06 07:47:49 +0200

    Fix equality_comparable_with and added meta_mixins.

--- include/experimental/fundamental/v3/strong/mixins/equality_comparable.hpp
+++ include/experimental/fundamental/v3/strong/mixins/equality_comparable.hpp
@@ -36,6 +36,22 @@ namespace std
         static_assert(is_same<Final, Other>::value==false, "Final and Other must be different");
         //! Forwards to the underlying value
         friend constexpr bool operator==(Final const& x, Other const& y) noexcept
+        { return x.underlying() == y;}
+        friend constexpr bool operator==(Other const& x, Final const& y) noexcept
+        { return y == x;}
+        //! Forwards to the underlying value
+        friend constexpr bool operator!=(Final const& x, Other const& y) noexcept
+        { return x.underlying() != y;}
+        friend constexpr bool operator!=(Other const& x, Final const& y) noexcept
+        { return y != x;}
+      };
+
+      template <class Final, class Other>
+      struct equality_comparable_with2 : equality_comparable<Final>
+      {
+        static_assert(is_same<Final, Other>::value==false, "Final and Other must be different");
+        //! Forwards to the underlying value
+        friend constexpr bool operator==(Final const& x, Other const& y) noexcept
         { return x.underlying() == y.underlying();}
         friend constexpr bool operator==(Other const& x, Final const& y) noexcept
         { return x.underlying() == y.underlying();}
@@ -58,6 +74,27 @@ namespace std
         template <class Other, typename = enable_if_t<Pred<Final, Other>::value>>
         friend constexpr bool operator!=(Final const& x, Other const& y) noexcept
         { return x.underlying() != y.underlying();}
+      };
+    }
+    namespace meta_mixin
+    {
+      template <class = void>
+      struct equality_comparable
+      {
+        template <class Final>
+        using type = mixin::equality_comparable<Final>;
+      };
+      template <class Other>
+      struct equality_comparable_with
+      {
+        template <class Final>
+        using type = mixin::equality_comparable_with<Final, Other>;
+      };
+      template <template <class, class> class Pred=mixin::is_compatible_with>
+      struct equality_comparable_with_if
+      {
+        template <class Final>
+        using type = mixin::equality_comparable_with_if<Final, Pred>;
       };
     }
   }
